e2b705d8783840924f8dab81251d2bde5907edd8
felipeparaizo <felipeparaizo229@gmail.com>
2026-09-02 20:36:28 -0400

    Initial

---
+++ COMMIT_EDITMSG.py
@@ -0,0 +1,11 @@
+print ("ola a todos")
+aluno = "felipe"
+#print ("ola ,",aluno,)
+idade = 19 
+#print("bem vindo,",aluno, "você tem,",idade,)
+cidade = "manaus"
+print("bem vindo,",aluno, "você tem,",idade, "e mora em,",cidade,)
+curso = "tecnico em informática"
+print("bem vindo,",aluno, "você tem,",idade, "e mora em,",cidade, "e faz o curso de ,",curso,)
+
+
---
+++ trabalho felipe ativ 3.py
@@ -0,0 +1,10 @@
+nome="felipe"
+idade=19 
+altura=1.77
+booleano=True
+
+
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(booleano))
